3e629afa63b85242b50db70115ad5cdb5b38b420
Selwob <66890383+Selwob@users.noreply.github.com>
2022-04-01 13:22:45 +0900

    added main option menu

---
+++ assets/js/options.js
@@ -0,0 +1,26 @@
+class Options {
+
+    // Render Options Menu
+    render() {
+        let menuEl = document.createElement('div');
+
+        menuEl.innerHTML = `
+            <h2>Options</h2>
+
+            <ul>
+                <li class="menu-item" id="options-input">Input</li>
+                <li class="menu-item" id="options-gameplay">Gameplay</li>
+                <li class="menu-item" id="options-gameplay">Multiplayer</li>
+                <li class="menu-item" id="options-sound">Sound</li>
+                <li class="menu-item" id="options-display">Display</li>
+                <li class="menu-item" id="options-language">Language</li>
+                <li class="menu-item" id="options-bethesda.net">Bethesda.net</li>
+                <li class="menu-item" id="options-credits">Credits</li>
+            </ul>
+        `
+
+        addOnClickHashChange(menuEl);
+
+        return menuEl
+    }
+}
