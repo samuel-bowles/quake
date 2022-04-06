ea88d138ed886872b53b93ce4f62ebeda1825dd3
Selwob <66890383+Selwob@users.noreply.github.com>
2022-04-06 23:03:51 +0900

    Added menu hover audio effect - needs permissions settings still

--- assets/js/soundEffects.js
+++ assets/js/soundEffects.js
@@ -1,1 +1,13 @@
-console.log("yo boi")
+const menuHover = new Audio("assets/audio/menu1.wav");
+
+
+function playSound() {
+    menuHover.play();
+}
+
+function hoverListener() {
+    const menuItem = document.querySelectorAll('.menu-item');
+    menuItem.forEach(item => {
+        item.addEventListener("mouseover", playSound)
+    })
+}
