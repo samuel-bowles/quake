cbbf0b365088a57d2fb909fa0a0e4b7b55582cd3
Selwob <66890383+Selwob@users.noreply.github.com>
2022-04-07 13:54:20 +0900

    Added Sounds to Buttons

--- assets/js/soundEffects.js
+++ assets/js/soundEffects.js
@@ -1,13 +1,33 @@
-const menuHover = new Audio("assets/audio/menu1.wav");
+// Add Audio Effects
+
+// Menu Hover
+const menuHoverSound = new Audio("assets/audio/menu1.wav");
+menuHoverSound.volume = 0.2;
+menuHoverSound.muted = true;
+// Menu Button Click
+const menuButtonSound = new Audio("assets/audio/menu2.wav");
+menuButtonSound.volume = 0.2;
+menuButtonSound.muted = true
 
 
-function playSound() {
-    menuHover.play();
+// Toggles Audio On and Off
+function toggleAudio() {
+        menuHoverSound.muted ? menuHoverSound.muted = false : menuHoverSound.muted = true;
+        menuButtonSound.muted ? menuButtonSound.muted = false : menuButtonSound.muted = true;
+}
+
+// Play menu audio effect
+function playMenu() {
+    menuHoverSound.play();
+}
+
+function playButton() {
+    menuButtonSound.play();
 }
 
 function hoverListener() {
     const menuItem = document.querySelectorAll('.menu-item');
     menuItem.forEach(item => {
-        item.addEventListener("mouseover", playSound)
+        item.addEventListener("mouseover", playMenu)
     })
 }
